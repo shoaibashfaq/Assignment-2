32430cdd314c75e6d43c7eb1e4d9dc0c6bc66ec7
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-06-13 18:03:37 -0400

    Added function3 and function4

--- Assignment2.js
+++ Assignment2.js
@@ -5,7 +5,7 @@ function forEach(array,callback){
         callback(array[i]);
     }
 }
-
+//nothing to return
 forEach(nums,(item)=>console.log(item+1))
 
 //function 2
@@ -18,4 +18,32 @@ function map(array,callback){
     return result;
 }
 
-map(nums,(item)=>item*2)
+const resultMap=map(nums,(item)=>item*2);
+console.log(resultMap);
+
+//function 3
+function filter(array,callback){
+    let result =[];
+    for (let i=0;i<array.length;i++){
+        if (callback(array[i])) result.push(array[i]);
+    };
+    return result;
+}
+
+const filterResult=filter(nums,(item)=>item%2);
+console.log(filterResult);
+
+// function4
+function some(array,callback){
+    for (let i=0;i<array.length;i++){
+        if (callback(array[i])) return true;
+    };
+}
+
+let hasEven=some(nums,(item)=>item%2===0);
+
+//function5
+
+
+
+
