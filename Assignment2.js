096a495d45cbbd2a88a42bf97612d95da7194051
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-06-14 09:09:43 -0400

    Adding Deep Comparision method

--- Assignment2.js
+++ Assignment2.js
@@ -244,6 +244,31 @@ function arrayToList(array) {
 
 
 //4. Deep Comparision
+function deepEqual(obj1,obj2) {
+    //if the deep comaprision return true even before we loop the key:value pairs
+    if (obj1 === obj2) return true;
+  
+    //to get around the (typeof null) == object 
+    if (typeof obj1 !== "object" || obj1 === null) return false;
+    
+    if (typeof obj2 !== "object" || obj2 === null) return false;
+
+  
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+  
+    if (keys1.length !== keys2.length) return false;
+  
+    for (let key of keys1) {
+        //the recursive call is used to make sure all the nested values are also the same
+        //since Object.keys() only gives top level values;
+      if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) return false;
+    }
+  
+    return true;
+  }
+
+  let obj = {here: {is: "an"}, object: 2};
 
 //Last Miscellaneauas
       
@@ -268,7 +293,7 @@ function moveZerosToEnd(nums) {
   }
   
 
-let numbers=[2,3,0,9,2,1,0,0];
+let numbers=[2,3,0,9,11,19,0,0];
 console.log(moveZerosToEnd(numbers));
 
 
